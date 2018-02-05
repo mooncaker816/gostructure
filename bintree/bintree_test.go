3aa07c4aea8adc512db1a2ad75a9230fb868accc
Mingle <MingleGu@gmail.com>
2018-02-05 17:27:52 +0800

    update

--- bintree/bintree_test.go
+++ bintree/bintree_test.go
@@ -699,3 +699,26 @@ func TestCopyI(t *testing.T) {
 	_ = c
 	_ = d
 }
+
+func TestMirror(t *testing.T) {
+	bt := new(BinTree)
+	root := bt.InsertAsRoot("k", nil)
+	i := bt.InsertAsLChild(root, "i", nil)
+	j := bt.InsertAsRChild(root, "j", nil)
+	h := bt.InsertAsRChild(i, "h", nil)
+	b := bt.InsertAsLChild(h, "b", nil)
+	g := bt.InsertAsRChild(h, "g", nil)
+	a := bt.InsertAsRChild(b, "a", nil)
+	e := bt.InsertAsLChild(g, "e", nil)
+	f := bt.InsertAsRChild(g, "f", nil)
+	c := bt.InsertAsLChild(e, "c", nil)
+	d := bt.InsertAsRChild(e, "d", nil)
+	bt.Print()
+	bt.Mirror()
+	bt.Print()
+	_ = j
+	_ = a
+	_ = f
+	_ = c
+	_ = d
+}

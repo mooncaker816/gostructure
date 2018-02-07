23f3cb7368644de3fb8ab2763d6a6cd798b563df
Mingle <MingleGu@gmail.com>
2018-02-07 21:27:09 +0800

    update

---
+++ avl/avl.go
@@ -0,0 +1,18 @@
+package avl
+
+import (
+	"github.com/mooncaker816/gostructure/bintree"
+	"github.com/mooncaker816/gostructure/bst"
+)
+
+type Avl struct {
+	*bst.Bst
+}
+
+func (avl *Avl) Insert(key, data interface{}) *bintree.Node {
+
+}
+
+func (avl *Avl) Remove(key interface{}) bool {
+
+}
